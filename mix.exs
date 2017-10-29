e48cc5b590227a83d051f3bceff17ad45ba8338e
Dariusz Gawdzik <dgawdzik@gmail.com>
2017-10-29 10:08:12 -0700

    Update deps

--- mix.exs
+++ mix.exs
@@ -55,9 +55,9 @@ defmodule ExContract.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
-      {:credo, "~> 0.7", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
+      {:dialyxir, "~> 0.5", only: :dev, runtime: false},
+      {:credo, "~> 0.8", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.18", only: :dev, runtime: false}
     ]
   end
 end
